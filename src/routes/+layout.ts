0e36f687f5b5ed38a6ef96fea8ad7b450a2b10d0
Tia Nguyen <tia.nkm03@gmail.com>
2025-07-29 10:31:20 +0100

    add vercel web analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,8 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+ 
+injectAnalytics({ mode: dev ? 'development' : 'production' });
+
+
+
 export const prerender = true
